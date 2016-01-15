b167dddbf385d604941869af324b3fb0a15976de
Phil Weir <phil.weir@numa.ie>
2016-01-15 10:22:40 +0000

    rel import at wrong level

--- gssa/src/gssa/families/elmer_libnuma.py
+++ gssa/src/gssa/families/elmer_libnuma.py
@@ -15,8 +15,8 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-from .family import Family
-from .parameters import convert_parameter
+from gssa.family import Family
+from gssa.parameters import convert_parameter
 
 
 import os
