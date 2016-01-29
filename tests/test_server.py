d91ebd9f0303fdbc1ee300862243a2637913fe5d
Phil Weir <phil.weir@numa.ie>
2016-01-29 14:50:19 +0000

    rebuild server for each test

--- tests/test_server.py
+++ tests/test_server.py
@@ -10,21 +10,14 @@ known_guid = str(uuid.uuid4())
 unknown_guid = str(uuid.uuid4())
 
 
-def make_coro(result):
-    @asyncio.coroutine
-    def it():
-        return result
-    return it
-
-
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="function")
 def definition():
     definition = MagicMock()
     definition.guid = known_guid
     return definition
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="function")
 def gsssc(definition):
     server_id = 'test-000000'
     database = MagicMock()
@@ -54,7 +47,7 @@ def test_init_succeeds(gsssc):
 
 @pytest.mark.asyncio
 def test_clean_succeeds(gsssc, definition):
-    definition.clean = make_coro(True)
+    definition.clean = asyncio.coroutine(lambda: True)
 
     result = yield from gsssc.doClean(known_guid)
 
@@ -66,3 +59,15 @@ def test_clean_fails_if_guid_unrecognised(gsssc):
     result = yield from gsssc.doClean(unknown_guid)
 
     assert(result is False)
+
+
+@pytest.mark.asyncio
+def test_start_succeeds(gsssc, definition):
+    simulate = MagicMock()
+    gsssc.doSimulate = asyncio.coroutine(simulate)
+    simulate.assert_called_with(known_guid)
+    gsssc._handle_simulation_done = MagicMock().assert_called_once()
+
+    result = yield from gsssc.doStart(known_guid)
+
+    assert(result is True)
